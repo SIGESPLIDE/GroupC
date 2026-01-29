95011886d4367076c51e92c73f6b814eff44bd53
h1y0-0 <215459185+h1y0-0@users.noreply.github.com>
2026-01-29 14:39:58 +0900

    dailyattendBeanの追加

---
+++ src/Bean/dailyattend.java
@@ -0,0 +1,32 @@
+package Bean;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+public class dailyattend implements Serializable {
+	LocalDateTime jstNow = LocalDateTime.now(ZoneId.of("Asia/Tokyo"));
+
+	private int studentId;
+	private LocalDateTime loginTime;
+	private LocalDateTime logoutTime;
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	public LocalDateTime getLoginTime() {
+		return loginTime;
+	}
+	public void setLoginTime(LocalDateTime loginTime) {
+		this.loginTime = loginTime;
+	}
+	public LocalDateTime getLogoutTime() {
+		return logoutTime;
+	}
+	public void setLogoutTime(LocalDateTime logoutTime) {
+		this.logoutTime = logoutTime;
+	}
+
+}
